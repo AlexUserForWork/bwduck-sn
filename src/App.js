8291551dfe290be8304dc060bf591a2fe1a50b7f
Kononiuk <hmasteraaa@gmail.com>
2024-11-20 20:18:06 +0200

    added duck

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import heroDuck from './hero-duck1.png';
+import duckV from './Comp 1_1.mp4';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className='App'>
+         <video className='main-img' id='introVideo' width='800' autoplay=''>
+            <source src={duckV} type='video/mp4' />
+            Video doesn't work because you sniffed too much glue.
+         </video>
+         <p className='main-text'>COMING SOON</p>
+      </div>
+   );
 }
 
 export default App;
